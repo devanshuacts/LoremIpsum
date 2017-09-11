ab03cd8be8281ef67cb1f4bb1160cf1b4d50e7c3
devanshuacts <Devanshu.ds.singh@gmail.com>
2017-09-11 19:04:34 +0530

    handling myaction post method by adding route of same name

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var things = require('./things.js')
 app.set('view engine', 'jade');
 app.set('views', './views')
 app.use(express.static('public/images'))
+app.use('/lorem_ipsum', express.static('public'))
 app.use('/things', things)
+
+var bodyparser = require('body-parser')
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}))
 
 app.use('/:name/:id([0-9]{5})', function(req, res, next){
     console.log("request received at time " + Date.now())
@@ -28,6 +35,11 @@ app.get('/dynamicView', function(req, res){
         name: "Devanshu",
         id: "6004"
     })
+})
+
+app.post('/myaction', function(req, res){
+    console.log("message received: " + req.body.email + " " + req.body.name)
+    res.send("Your request has been received " + req.body.name)
 })
 
 app.listen(4000)
